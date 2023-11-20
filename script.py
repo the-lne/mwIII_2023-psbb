b29797a72f0e686170227ee5e34fea03d6f259ee
the-lne <117259028+the-lne@users.noreply.github.com>
2023-11-19 23:54:44 -0600

    Update script.py

--- script.py
+++ script.py
@@ -2,35 +2,21 @@ import time
 import random
 import mouse
 from pynput.keyboard import Controller
-keyboard = Controller() 
-
-
-#init (get mouse position)
-#begin loop
-
-#issues: figure out how to determine whether a game is going or not
-
-#mouse.get_position()
-#mouse.move(125, 50)
-#mouse.move(150, 150, duration = 1.0)
-#mouse.click('left') 
-#mouse.click('right')    
-#mouse.click('middle')   
-#mouse.wheel(5)
-#mouse.wheel(-3)
-
+import win32api, win32con
 
 movement = ["s", "a", "d", "w", "w", "a", "d", "w", "w"]
+keyboard = Controller() 
 
-# begin loop! 
 time.sleep(5)
-#for i in range(1,10):
+
 while(1):
     currentKey = int(random.uniform(0,8))
     keyboard.press(movement[currentKey])
     keyboard.press('f')
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(random.uniform(0,1600)), int(random.uniform(0,720)), 0, 0)
     for j in range(int(random.uniform(10,20))):
         mouse.click('left') 
+        time.sleep(random.uniform(0,1)
     mouse.wheel(random.uniform(0,3))
     time.sleep(random.uniform(0, 2))
-    keyboard.release(movement[currentKey])
+    keyboard.release(movement[currentKey]
