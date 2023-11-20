e57fd5a00927e0840b7c02165edc81afe46a193e
the-lne <117259028+the-lne@users.noreply.github.com>
2023-11-20 00:12:01 -0600

    Update script.py

--- script.py
+++ script.py
@@ -5,18 +5,24 @@ from pynput.keyboard import Controller
 import win32api, win32con
 
 movement = ["s", "a", "d", "w", "w", "a", "d", "w", "w"]
+
+
 keyboard = Controller() 
-
 time.sleep(5)
-
+flipbit = 0
 while(1):
     currentKey = int(random.uniform(0,8))
     keyboard.press(movement[currentKey])
     keyboard.press('f')
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(random.uniform(0,1600)), int(random.uniform(0,720)), 0, 0)
-    for j in range(int(random.uniform(10,20))):
-        mouse.click('left') 
-        time.sleep(random.uniform(0,1)
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 
+            int(random.uniform(0,150)), 0, 0, 0)
+    if(flipbit):
+        for j in range(int(random.uniform(2,3))):
+            mouse.click('left') 
+            time.sleep(random.uniform(0,1))
+    else
+        keyboard.press('e')
     mouse.wheel(random.uniform(0,3))
-    time.sleep(random.uniform(0, 2))
-    keyboard.release(movement[currentKey]
+    time.sleep(random.uniform(0, 4))
+    keyboard.release(movement[currentKey])
+    !flipbit
