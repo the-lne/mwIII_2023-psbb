97905277912abe01cf2377d34667a19af2462956
the-lne <117259028+the-lne@users.noreply.github.com>
2023-11-20 01:13:30 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 # To run this, you need to remap the chat box to be something 
 # other than the enter key otherwise you will type 'wasd' 
 # throughout the match. Other than that, you should have default 
-# keybindings.
+# keybindings. Obviously, pip and some modules may need to be 
+# installed.
 
 import time
 import random
@@ -15,22 +16,22 @@ time.sleep(5)
 movement = ["s", "a", "d", "w", "w", "a", "d", "w", "w"]
 looking  = [1600, 500, 200, 25, 1200, 700, 800, 1000, 3000, 300]
 
-Controller = Controller() 
+keyboard = Controller() 
 flipbit = 0
 
 while(1):
     # skip death cutscene or select gun on match startup
-    Controller.press(Key.enter)
-    Controller.release(Key.enter)
-    Controller.press('f')
-    Controller.release('f')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    keyboard.press('f')
+    keyboard.release('f')
 
-    # select random values for Controller presses and mouse movements
+    # select random values for keyboard presses and mouse movements
     currentKey = int(random.uniform(0, --len(movement)))
     currentLook = int(random.uniform(0, --len(looking)))
 
     # use the random values that were just selected
-    Controller.press(movement[currentKey])
+    keyboard.press(movement[currentKey])
     win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 
             looking[currentLook], 0, 0, 0)
     
@@ -41,18 +42,18 @@ while(1):
     if(flipbit):
         for j in range(int(random.uniform(1, 5))):
             mouse.click('left') 
-            Controller.press('q')
+            keyboard.press('q')
             time.sleep(random.uniform(0,1))
-            Controller.release('q')
+            keyboard.release('q')
     else:
-        Controller.press('e')
+        keyboard.press('e')
         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, 0, 100, 0)
-        Controller.release('e')
+        keyboard.release('e')
         win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 0, 0, -100, 0)
 
     # sleeping allows for attempted map traversal while pressing movement[currentKey]
     time.sleep(random.uniform(0, 2))
-    Controller.release(movement[currentKey])
+    keyboard.release(movement[currentKey])
 
     # flip the bit that determines whether you shoot or throw a grenade
     if(flipbit == 1):
