7b661e9def02d830ec1227026a7fe5e38a318a52
the-lne <117259028+the-lne@users.noreply.github.com>
2023-11-20 02:19:54 -0600

    Update script.py

--- script.py
+++ script.py
@@ -14,10 +14,8 @@ keyboard = Controller()
 flipbit = 0
 
 while(1):
-    # skip death cutscene or select gun on match startup
-    keyboard.press(Key.enter)
+    # skip death cut scene
     keyboard.press('f')
-    keyboard.release(Key.enter)
     keyboard.release('f')
 
     # select random values for keyboard presses and mouse movements
@@ -32,20 +30,17 @@ while(1):
     # switch weapons
     mouse.wheel(random.uniform(0, 3))
 
-    # deploy equipment
-    keyboard.press('x')
-    keyboard.release('x')
-
     # decided if its time to throw a grenade or shoot
-    if(flipbit == 1):
-        for j in range(int(random.uniform(1, 5))):
+    if(flipbit == 0):
+        for j in range(int(random.uniform(2, 5))):
             mouse.click('left') 
             keyboard.press('q')
             time.sleep(random.uniform(0,1))
             keyboard.release('q')
+            mouse.click('left') 
     else:
         keyboard.press('e')
-        time.sleep(random.uniform(0,2))
+        time.sleep(random.uniform(1,2))
         keyboard.release('e')
 
     # sleeping allows for attempted map traversal while pressing movement[currentKey]
@@ -53,7 +48,7 @@ while(1):
     keyboard.release(movement[currentKey])
 
     # flip the bit that determines whether you shoot or throw a grenade
-    if(flipbit == 1):
-        flipbit = 0
-    else:
+    if(flipbit == 0):
         flipbit = 1
+    else:
+        flipbit = 0
